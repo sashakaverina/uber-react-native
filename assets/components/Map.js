a54184b843c99e857485bf988adff239f12921a1
Sasha Kaverina <sasha.kaverina@lewagon.org>
2021-11-21 17:31:01 +0900

    map directions work

--- assets/components/Map.js
+++ assets/components/Map.js
@@ -2,15 +2,35 @@ import React from 'react';
 import { View, Text } from 'react-native';
 import MapView, { Marker } from 'react-native-maps';
 import tw from 'tailwind-react-native-classnames';
-import { selectOrigin } from '../../slices/navSlice';
+import { selectDestination, selectOrigin } from '../../slices/navSlice';
 import { useSelector } from 'react-redux';
+import MapViewDirections from 'react-native-maps-directions';
+import { GOOGLE_MAPS_APIKEY } from "@env";
+import { useRef } from 'react';
+import { useEffect } from 'react';
 
 
 
 const Map = () => {
  const origin = useSelector(selectOrigin);
+ const destination = useSelector(selectDestination);
+ const mapRef = useRef(null);
+
+ 
+
+ useEffect(() => {
+   
+   if (!origin || !destination) return;
+   
+   mapRef.current.fitToSuppliedMarkers(['origin', 'destination'], {
+    edgePadding: { top: 50, right: 50, left: 50, bottom: 50}, 
+   });
+     
+  }, [origin, destination]);
+
   return (
     <MapView
+      ref={mapRef}
       style={tw `flex-1`}
       mapType="mutedStandard"
       initialRegion={{
@@ -20,6 +40,17 @@ const Map = () => {
         longitudeDelta: 0.005,
       }}
     >
+      {origin && destination && (
+        <MapViewDirections
+          origin={origin.description}
+          destination={destination.description}
+          apikey={GOOGLE_MAPS_APIKEY}
+          strokeColor="black"
+          strokeWidth={3}
+          onFail={error => console.error(error)}
+        />
+        
+      )}
     {origin?.location && (
       <Marker
       coordinate={{
@@ -29,6 +60,20 @@ const Map = () => {
       title="Origin"
       description={origin.description}
       identifier="origin"
+
+      
+      />
+    )}
+
+      {destination?.location && (
+      <Marker
+      coordinate={{
+        latitude: destination.location.lat,
+        longitude: destination.location.lng,
+      }}
+      title="Destination"
+      description={destination.description}
+      identifier="destination"
 
       
       />
